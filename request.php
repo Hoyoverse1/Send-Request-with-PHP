6c468729f5939a80f6fa08db029b786e074dfee8
CaiLin <121320505+Hoyoverse1@users.noreply.github.com>
2023-04-09 20:24:01 +0700

    Create request.php

---
+++ request.php
@@ -0,0 +1,12 @@
+$url = 'https://www.contohwebsite.com';
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($ch);
+
+echo $result;
+
+curl_close($ch);
